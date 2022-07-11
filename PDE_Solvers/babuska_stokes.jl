7950b20718829c42042764c054c47193dddd2ae6
Mikkel Metzsch Jensen <metzschjensen@gmail.com>
2022-07-11 16:19:35 +0200

    implemented first draft for babuska stokes, but have little understanding and doesn't work ofc

--- PDE_Solvers/babuska_stokes.jl
+++ PDE_Solvers/babuska_stokes.jl
@@ -18,36 +18,34 @@ end
 
 
 
-function babuska_stokes_solver(model, pgs_dict, f0, g0, write=false)
+function babuska_stokes_solver(model, pgs_dict, f0, g0, h0, write=false)
 
-    # Boundary conditions
+    # --- Boundary conditions --- #
     λ_diri_tags = pgs_tags(pgs_dict, [3]) # goes into Multiplier 
     λ_corner_tags = pgs_tags(pgs_dict, [7, 8])
     strong_diri_tags = pgs_tags(pgs_dict, [1, 2, 4, 5, 6, 7, 8]) # goes intro trial space
     neumann_tags = Vector{Int}()
-
-
-    # Triangulation and spaces
+    
+    # --- Triangulation and spaces --- #
     Ω = Triangulation(model)
     ΓD = BoundaryTriangulation(model, tags=λ_diri_tags)
     ΓN = BoundaryTriangulation(model, tags=neumann_tags)
-
+    
     order = 2
     reffeᵤ = ReferenceFE(lagrangian, VectorValue{2,Float64}, order)
     reffeₚ = ReferenceFE(lagrangian, Float64, order - 1, space=:P)
     reffeₘ = ReferenceFE(lagrangian, VectorValue{2,Float64}, order)
-
+    
     δV = TestFESpace(Ω, reffeᵤ, conformity=:H1, dirichlet_tags=strong_diri_tags) # Velocity
     δM = TestFESpace(Ω, reffeₚ, conformity=:L2, constraint=:zeromean) # Pressure
     δΛ = TestFESpace(ΓD, reffeₘ, conformity=:H1, dirichlet_tags=λ_corner_tags) # Multiplier
-
-    V = TrialFESpace(δV, g0)
-    M = TrialFESpace(δP)
-    Λ = TrialFESpace(δM, VectorValue(0, 0))
-
+    
+    V = TrialFESpace(δV, g0[1])
+    M = TrialFESpace(δM)
+    Λ = TrialFESpace(δΛ, VectorValue(0, 0))
+    
     δW = MultiFieldFESpace([δV, δM, δΛ]) # Test spaces
     W = MultiFieldFESpace([V, M, Λ]) # Trial spaces
-
 
     degree = 2 * order #
     dΩ = Measure(Ω, degree)
@@ -55,18 +53,17 @@ function babuska_stokes_solver(model, pgs_dict, f0, g0, write=false)
     dΓN = Measure(ΓN, degree)
     nD = get_normal_vector(ΓD)
 
-    # Weak formulation 
-    μ = 1.0
-    ρ = 1.0 # Hardcoded for now
-    ϵ(u) = 1 / 2 * (∇(u) + transpose(∇(u)))
+    # --- Weak formulation --- #
+    μ = 1.0; ρ = 1.0 # Hardcoded for now
+    p0 = g0[2]
 
+    ϵ(u) = 1 / 2 * (∇(u) + transpose(∇(u)))
     t(n) = VectorValue(n[2], -n[1])
     c(u, λ) = ∫((u ⋅ t(nD)) * λ) * dΓD
 
-    a((u, p, λ), (v, q, η)) = ∫(2 * μ * ϵ(u) ⊙ ϵ(v) - p * (∇ ⋅ v) ± q * (∇ ⋅ u) + c(v, λ)) * dΩ ± c(u, η)
+    a((u, p, λ), (v, q, η)) = ∫(2 * μ * ϵ(u) ⊙ ϵ(v) - p * (∇ ⋅ v) + q * (∇ ⋅ u) + c(v, λ)) * dΩ + c(u, η)
     # a((u, p, λ), (v, q, η)) = ∫(2 * μ * ϵ(u) ⊙ ϵ(v) - p * (∇ ⋅ v) ± q * (∇ ⋅ u) + c(v, λ)) * dΩ ± c(u, η)
     b((v, q, η)) = ∫(ρ * v ⋅ f0) * dΩ - ∫(p0 * nD ⋅ v) * dΓD
-
 
     # --- Solve --- #
     op = AffineFEOperator(a, b, W, δW)
@@ -97,4 +94,4 @@ g0 = (u0, p0)
 
 
 model, pgs_dict = create_unit_box(1, false)
-babuska_stokes_solver(model, pgs_dict, f0, g0, h0)
+babuska_stokes_solver(model, pgs_dict, f0, g0, σ)
