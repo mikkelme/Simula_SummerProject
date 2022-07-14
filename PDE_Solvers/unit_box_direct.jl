7aa2eef922b00ceafe61c86f483c7c8b76718354
Mikkel Metzsch Jensen <metzschjensen@gmail.com>
2022-07-14 10:04:47 +0200

    added coupled box to unit_box_direct

--- PDE_Solvers/unit_box_direct.jl
+++ PDE_Solvers/unit_box_direct.jl
@@ -10,12 +10,14 @@ function pgs_tags(pgs_dict, tags)
 end
 
 
+"""
+Unit square (sidelength = 1)
+# Physical tags
+   (8)--3--(7)
+ 4→ |      | ←2
+   (5)--1--(6)
+"""
 function create_unit_box(lc, view=false)
-    # Physical tags
-    # (8) 3 (7)
-    #  4     2
-    # (5) 1 (6)
-
     gmsh.initialize(["", "-clscale", string(lc)])
 
     A = gmsh.model.occ.addPoint(0, 0, 0)
@@ -60,8 +62,6 @@ function create_unit_box(lc, view=false)
     end
     
     
-
-    
     
     model, pgs_dict = direct_wiring(gmsh)
     gmsh.finalize()
@@ -71,11 +71,98 @@ end
 
 
 
-if abspath(PROGRAM_FILE) == @__FILE__
-    lc = 0.1
-    lc = 2
-    create_unit_box(lc, true)
+"""
+Unit square (sidelength = 1) with dividing line from (0, 0.5) to (1, 0.5)
+Physical tags
+  (12)--7--(12)
+ 5→ |       | ←6
+  (10)--4--(11)
+ 2→ |       | ←3
+   (8)--1--(9)
+
+"""
+function create_coupled_box(lc, view=false)
+    gmsh.initialize(["", "-clscale", string(lc)])
+
+    A = gmsh.model.occ.addPoint(0, 0, 0)
+    B = gmsh.model.occ.addPoint(1, 0, 0)
+    C = gmsh.model.occ.addPoint(1, 1, 0)
+    D = gmsh.model.occ.addPoint(0, 1, 0)
+    ΓL = gmsh.model.occ.addPoint(0, 0.5, 0)
+    ΓR = gmsh.model.occ.addPoint(1, 0.5, 0)
+
+    P = Array{Int32,2}(undef, 2, 4)
+    line = Array{Int32,2}(undef, 2, 4)
+    surf = Array{Int32,1}(undef, 2)
+
+
+    PD = [A, B, ΓR, ΓL] # Points Darcy
+    PS = [ΓL, ΓR, C, D] # Points Stokes
+    P[1, :] = PD
+    P[2, :] = PS
+    
+    Γline = gmsh.model.occ.addLine(ΓL, ΓR)
+   
+    for i in 1:2
+        for j in 1:4
+            from, to = P[i, 1+(j-1)%4], P[i, 1+j%4]
+            if (from, to) == (ΓL, ΓR)
+                line[i,j] = Γline
+            elseif (from, to) == (ΓR, ΓL)
+                line[i,j] = -Γline
+
+            else 
+                line[i, j] = gmsh.model.occ.addLine(from, to)
+            end   
+        end
+        loop = gmsh.model.occ.addCurveLoop([line[i, 1], line[i, 2], line[i, 3], line[i, 4]])     
+        surf[i] = gmsh.model.occ.addPlaneSurface([loop])
+        gmsh.model.occ.synchronize()
+        gmsh.model.mesh.setTransfiniteSurface(surf[i]) # Ordering to nice triangles og squares
+    end
+
+    # Physical group
+    gmsh.model.addPhysicalGroup(1, [line[1, 1]], 1)
+    gmsh.model.addPhysicalGroup(1, [line[1, 4]], 2)
+    gmsh.model.addPhysicalGroup(1, [line[1, 2]], 3)
+    gmsh.model.addPhysicalGroup(1, [Γline], 4)
+    gmsh.model.addPhysicalGroup(1, [line[2, 4]], 5)
+    gmsh.model.addPhysicalGroup(1, [line[2, 2]], 6)
+    gmsh.model.addPhysicalGroup(1, [line[2, 3]], 7)
+
+    gmsh.model.addPhysicalGroup(0, [A], 8)
+    gmsh.model.addPhysicalGroup(0, [B], 9)
+    gmsh.model.addPhysicalGroup(0, [ΓL],10)
+    gmsh.model.addPhysicalGroup(0, [ΓR],11)
+    gmsh.model.addPhysicalGroup(0, [D], 12)
+    gmsh.model.addPhysicalGroup(0, [C], 13)
+
+    # # gmsh.option.setNumber("Mesh.RecombineAll", 1) # For square mesh
+    gmsh.model.mesh.generate(2)
+
+    if view
+        gmsh.fltk.initialize()
+        gmsh.fltk.run()
+    end
+
+
+     
+    model, pgs_dict = direct_wiring(gmsh)
+    gmsh.finalize()
+    gmsh.finalize()
+
+
 end
+
+
+
+# create_unit_box(2, true)
+create_coupled_box(2, true)
+
+# if abspath(PROGRAM_FILE) == @__FILE__
+#     lc = 0.1
+#     lc = 2
+# end
 
 
 
